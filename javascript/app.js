e446be7de93a78d2d36707ad3fcd40ea83f1fbd0
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-05 21:58:00 +0100

    constant variable

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,7 @@ console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
 */
 
-var favoriteFood = "pizza";
+/*var favoriteFood = "pizza";
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
@@ -12,4 +12,10 @@ console.log(numOfSlices);
 let changeMe = true;
 changeMe = false;
 console.log(changeMe);
+*/
 
+//constant variable
+const entree = "Enchiladas";
+console.log(entree);
+
+entree = "Tacos";
