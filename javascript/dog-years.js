306675725aef14c80eb0d57f539a611090a53374
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-06 16:52:01 +0100

    complete codes

--- javascript/dog-years.js
+++ javascript/dog-years.js
@@ -1,9 +1,23 @@
-const myAge = 30; // My current age in human years
+const myAge = 40; // My current age in human years
 
 // Task 2: Create a variable named earlyYears and save the value 2 to it. The first two years of a dog's life count as 10.5 dog years each
-let earlyYears = 2;
+let earlyYears = 2; 
 
 // Task 3: Use the multiplication assignment operator to multiply the value saved to earlyYears by 10.5 and reassign it to earlyYears.
 earlyYears *= 10.5;
 
-let laterYears = earlyYears - 2;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+
+//calculate my age
+let myAgeInDogsYears = earlyYears + laterYears 
+
+// Replace 'Alireza' with your name
+let myName = 'Alireza'.toLowerCase(); 
+
+// Task 8: Write a console.log statement that displays your name and age in dog years.
+console.log(`My name is ${myName}. I am ${myAge}.`);
