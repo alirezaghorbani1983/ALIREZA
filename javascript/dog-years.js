5f840830b8dc458c8264b863d1b3ae31a4e3c5d6
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-06 15:22:52 +0100

    dog years convertion

---
+++ javascript/dog-years.js
@@ -0,0 +1,9 @@
+const myAge = 30; // My current age in human years
+
+// Task 2: Create a variable named earlyYears and save the value 2 to it. The first two years of a dog's life count as 10.5 dog years each
+let earlyYears = 2;
+
+// Task 3: Use the multiplication assignment operator to multiply the value saved to earlyYears by 10.5 and reassign it to earlyYears.
+earlyYears *= 10.5;
+
+let laterYears = earlyYears - 2;
