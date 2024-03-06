2060b95275a41fda27b3312af01b7bd7da2bb6f7
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-06 18:26:14 +0100

    compare operators

--- javascript/app.js
+++ javascript/app.js
@@ -56,3 +56,12 @@ console.log(lostDollar);
 //concatenation
 let favoriteAnimal = "dog";
 console.log('My favorite animal: ' + favoriteAnimal);
+
+
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+    console.log('Time to eat!')
+} else {
+    console.log('We can eat later!')
+}
