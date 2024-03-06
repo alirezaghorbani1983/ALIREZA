b2f3408a5cc1b361c7474fff89174a43d62c8494
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-06 12:21:21 +0100

    fahrenheit to ceslieus en rounding down

--- javascript/kelvin-weather.js
+++ javascript/kelvin-weather.js
@@ -2,3 +2,9 @@
 const kelvin = 293;
 //kelvin convert to celsius
 const celsius = kelvin - 273;
+
+// Conversion celsius to Fahrenheit
+const fahrenheit = celsius * (9 / 5) + 32; 
+
+//Rounding down Fahrenheit temperature
+fahrenheit = Math.floor(fahrenheit);
