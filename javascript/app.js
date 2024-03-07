68963eedb1b89c65217b106ab80828351b26e624
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-07 13:01:00 +0100

    switch statement

--- javascript/app.js
+++ javascript/app.js
@@ -141,6 +141,7 @@ if (favoritePhrase === 'Love That!') {
 favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
 */
 
+/*
 let season = 'summer';
 
 if (season === 'spring') {
@@ -153,4 +154,23 @@ if (season === 'spring') {
   console.log('It\'s sunny and warm because it\'s summer!')
 } else {
   console.log('Invalid season.');
+}
+*/
+
+
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+    case 'first place':
+        console.log('You get the gold medal!');
+        break;
+    case 'second place':
+        console.log('You get the silver medal!');
+        break;
+    case 'third place':
+        console.log('You get the bronze medal!');
+        break;
+    default:
+        console.log('No medal awarded.')
+        break;
 }
