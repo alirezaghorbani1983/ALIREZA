3179d4efba6f3e75ae369a085a557e35e3740ac2
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-11 20:25:13 +0100

    final code

--- javascript/race-day.js
+++ javascript/race-day.js
@@ -1,8 +1,5 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-console.log(raceNumber);
-
-
 let earlyRegister = true;
 
 let runnerAge = 10;
@@ -13,8 +10,8 @@ if (earlyRegister && runnerAge > 18){
 
 if (earlyRegister && runnerAge > 18){
   console.log(`Race begins at 9:30 AM . Your race number is : ${raceNumber}.`);
-} else if (runnerAge > 18 && earlyRegister === false) {
-    console.log('Begin at 11:00 AM')
-} else if (runnerAge < 18) {
-    console.log('Begin at 12:30 AM')
+} else if(!earlyRegister && runnerAge > 18){
+  console.log(`Race begins at 11:00 AM . Your race number is : ${raceNumber}.`);
+}else if(runnerAge < 18){
+  console.log(`Race begins at 11:00 AM . Your race number is : ${raceNumber}.`);
 }
