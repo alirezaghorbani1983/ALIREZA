720ba31196afdf6ea640d490755fd54f7029ad87
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-12 18:20:19 +0100

    variables

--- webprog/index.php
+++ webprog/index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        echo "new";
-        echo "<br>";
-        echo "hello world"
-    ?>
-</body>
-</html>
+<?php
+$name = "Ali";
+echo $name;
+?>
