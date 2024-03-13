bfcdda2b405a2277a78ddbb30e3d29887a11b1c7
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-13 12:48:55 +0100

    work with string

--- webprog/index.php
+++ webprog/index.php
@@ -6,12 +6,16 @@
 // $y = 10;
 
 // var_dump((float)$y);
-$s = 2.99;
-var_dump(is_numeric($s));
+// $s = 2.99;
+// var_dump(is_numeric($s));
 
-echo rand(0,10) . PHP_EOL;
+// echo rand(0,10) . PHP_EOL;
 
-echo floor(6.9);
+// echo floor(6.9);
 
+$str = 'hello world';
+//echo str_world_count($str);
+$str[3] = 'm';
+echo $str;
 
 ?>
