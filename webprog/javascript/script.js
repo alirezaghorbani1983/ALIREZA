c878d37435b6379ecfa0192dcb9edd3eefd51350
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-20 22:25:28 +0100

    types of console

--- webprog/javascript/script.js
+++ webprog/javascript/script.js
@@ -1,1 +1,12 @@
-alert('test');
+console.log('welkom');
+
+console.log(45);
+
+console.error('error');
+
+console.warn('warning');
+
+console.group('test');
+console.group('ali');
+console.group('Mike');
+console.groupEnd();
