b90dc346529253b68eca51d848c76abbb265c833
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-21 16:17:52 +0100

    post checkbox

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -1,15 +1,24 @@
 <?php
 
-if(isset($_POST['sned'])){
-    echo $_POST['email'];
-}
+// if(isset($_POST['sned'])){
+//     echo $_POST['email'];
+// }
 
+if (isset($_POST['send'])) {
+    echo '<pre>';
+    print_r($_POST);
+}
 ?>
 
 <form action="./index.php" method="post">
-    <input type="text" name="email">
+    <!-- <input type="text" name="email">
     <br>
     <input type="text" name="password">
+    <br> -->
+
+    <input type="checkbox" name="php"> php;
+    <br>
+    <input type="checkbox" name="laravel" value="laravel"> laravel;
     <br>
     <button type="submit" name="send">send</button>
 </form>
