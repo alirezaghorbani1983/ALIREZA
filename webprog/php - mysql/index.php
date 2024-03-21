4410d45e55009237a1f41ce074dfb578ffec2d7b
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-21 20:07:18 +0100

    email and password validate php

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -4,16 +4,35 @@
 //     echo $_POST['email'];
 // }
 
+$errors =[];
 if (isset($_POST['send'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     if(empty($email)){
-        echo "email required";
+        array_push($errors, "email required");
+    } elseif (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "invalid email");
     }
 
+    if(empty($password)){
+        array_push($errors, "password required");
+    } elseif(strlen($password) < 5) {
+        array_push($errors, "password cannot be less than 5 characters");
+    }    
+
+    if(empty($errors)){
+        echo $email, $password;
+    } 
 }
 ?>
+
+<div style="color : red">
+    <?php foreach($errors as $error): ?>
+        <p><?= $error ?></p>
+        <?php endforeach ?>
+
+</div>
 
 <form action="./index.php" method="post">
     <input type="text" name="email">
