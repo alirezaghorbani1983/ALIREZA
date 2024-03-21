366e6b93537a54ae1c57d9b4e4b2e14634c6083f
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-21 16:05:39 +0100

    post methode

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -1,120 +1,12 @@
-
-// $x = 5;
-
-// var_dump($x);
-
-// $y = 10;
-
-// var_dump((float)$y);
-// $s = 2.99;
-// var_dump(is_numeric($s));
-
-// echo rand(0,10) . PHP_EOL;
-
-// echo floor(6.9);
-
-// $str = 'hello world';
-// //echo str_world_count($str);
-// // $str[3] = 'm';
-// // echo $str;
-
-// //echo substr($str, 6, 3);
-// echo strpos($str, 'o', 5);
-// echo strripos($str, 'O');
-// $names = ['ali', 'liam', 'sara'];
-
-// rsort($names);
-
-
-//print_r($names);
-
-// $x = 5;
-// switch($x){
-//     case 1: 
-//         echo "x is 1";
-//         break;
-//     case 2:
-//         echo "x is 2";
-//         break;
-//     case 3: 
-//         echo "x is 3";
-//         break;
-//     default:
-//         echo "x is $x";
-//         break;
-// }
-
-
-// for($i = 1; $i <= 5; $i++){
-//     echo $i . PHP_EOL;
-
-//     for($j = 0; $j <= 3; $j++){
-//         echo $i . '-' . $j . PHP_EOL;
-//     }
-// }
-
-// for($i = 1; $i <= 5; $i++){
-//     if($i == 3){
-//         echo "Breaking ...";
-//         break;
-//     }
-//     echo $i . PHP_EOL;
-// }
-// declare(strict_types = 1);
-
-
-// function add(int|float|string $x, int $y)
-// {
-//     return $x + $y;
-// }
-// $result = add('5', 6);
-// echo $result;
-
-// function sum($x, $y, ...$numbers)
-// {
-//     print_r($numbers);
-
-//     return $x + $y;
-// }
-
-// echo sum(10, 12, 4, 8, 9);
-
-// $x = 100; //global
-// function demo(){
-//     global $x;
-//     $y = 50; //local
-//     echo $x;
-// }
-
-// demo();
-
-// $x = 20;
-
-// $greet = function ($name) use($x) {
-//     // global $x;
-//     return "hello $name - $x";
-// };
-
-// echo $greet("ali");
-
-// $y = 50;
-
-// $greet = fn ($name) => "hello $name - $y";
-
-// echo $greet("liam");
-// require "./other.php";
-// require_once "./other.php";
-// require "test.php";
-
-// example();
-
 <?php
-echo "<pre>";
-    print_r($_GET);
+
+if(isset($_POST['sned'])){
+    echo $_POST['email'];
+}
 
 ?>
 
-<form action="./index.php">
+<form action="./index.php" method="post">
     <input type="text" name="email">
     <br>
     <input type="text" name="password">
