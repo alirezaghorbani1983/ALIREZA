6bea38bf87f0694cd57c9a551e2d8966ba7f7c32
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-21 16:39:22 +0100

    different inputs

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -16,9 +16,24 @@ if (isset($_POST['send'])) {
     <input type="text" name="password">
     <br> -->
 
-    <input type="checkbox" name="php"> php;
+    <!-- <input type="checkbox" name="php"> php;
     <br>
     <input type="checkbox" name="laravel" value="laravel"> laravel;
+    <br> -->
+    <!-- <input type="radio" name="vue.js"> Vue.js;
+    <br>
+    <input type="radio" name="raect.js" value="raect"> React.js;
+    <br> -->
+
+    <!-- <textarea name="textarea" id="" cols="30" rows="10">welkom</textarea>
+    <br> -->
+
+    <select multiple name="lang[]" id="">
+        <option value="java">Java</option>
+        <option value="numby" >Numby</option>
+        <option value="python">python</option>
+
+    </select>
     <br>
     <button type="submit" name="send">send</button>
 </form>
