c3382e96c137458374d77d70de31f0ef7a953fcd
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-22 20:00:48 +0100

    delet cookie for browser

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -1,11 +1,11 @@
 <?php
 
-setcookie("user", 'alireza ghorbani', time() + 3600);
+setcookie("user", 'alireza ghorbani', time() - 3600);
 
-if(isset($_COOKIE['user'])){
-    echo $_COOKIE['user'];
-} else {
-    echo "cookie is not set";
-}
+// if(isset($_COOKIE['user'])){
+//     echo $_COOKIE['user'];
+// } else {
+//     echo "cookie is not set";
+// }
 
 ?>
