76a5c4c6ac42d31f2edfb08e3e14af04b125829b
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-23 03:56:55 +0100

    type conversion

--- webprog/javascript/script.js
+++ webprog/javascript/script.js
@@ -2,13 +2,47 @@
 
 // console.log(firstNmae, typeof firstNmae); 
 
-const age = 25;
+// const age = 25;
 
-console.log(age, typeof age);
+// console.log(age, typeof age);
 
-const user = {
-    name :'ali',
-    age : 25
-}
+// const user = {
+//     name :'ali',
+//     age : 25
+// }
 
-console.log(typeof user);
+// console.log(typeof user);
+
+// let value = '9';
+
+// console.log();
+
+//covert strig to number
+// value = parseInt(value);
+
+// value = +value;
+
+// value = Number(value);
+
+// convert number tot string 
+
+// let value = 30;
+// value = value.toString();
+
+// console.log(value.__proto__);
+
+// value = String(value);
+
+//covert string to decimal
+
+// let value = '5.6';
+
+// value = parseFloat(value);
+
+// convert number to boolean
+let value = 0;
+
+value = Boolean(value);
+console.log(value, typeof value);
+
+
