1004ba3c6b865127a1b2c46448ee9bff4739089d
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-25 22:43:18 +0100

    another way for write in file

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -1,10 +1,13 @@
 <?php
 
-$file = fopen('log.txt', 'a');
+$file = fopen('log.txt', 'w');
 
 fwrite($file, 'ali');
 
 fclose($file);
+
+
+file_put_contents("log.txt", 'php ...', FILE_APPEND)
 
 ?>
 
