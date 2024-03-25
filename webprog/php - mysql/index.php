ee7815bbf61b2c1bb702a0fd379a937837b2c959
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-25 23:54:40 +0100

    other function for read a file

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -2,9 +2,13 @@
 
 $file = fopen('log.txt', 'r');
 
-$content = fread($file, 2);
+$content = fread($file, filesize('log.txt'));
 
 echo $content;
+
+fclose($file);
+
+echo file_get_contents('log.txt');
 
 ?>
 
