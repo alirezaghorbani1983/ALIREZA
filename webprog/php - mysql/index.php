2a0e0d68c3068c8cef4d98aa22c24d6d0f5d93e9
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-26 00:19:22 +0100

    copy , rename en delete a file

--- webprog/php - mysql/index.php
+++ webprog/php - mysql/index.php
@@ -1,14 +1,24 @@
 <?php
 
-$file = fopen('log.txt', 'r');
+// $file = fopen('log.txt', 'r');
 
-$content = fread($file, filesize('log.txt'));
+// $content = fread($file, filesize('log.txt'));
 
-echo $content;
+// echo $content;
 
-fclose($file);
+// fclose($file);
 
-echo file_get_contents('log.txt');
+// echo file_get_contents('log.txt');
+
+//copy file
+
+// copy('log.txt', 'log_copy.txt');
+
+//rename file
+
+// rename('log_copy.txt', 'log_renamed.txt');
+
+unlink('log_renamed.txt');
 
 ?>
 
