c2f37db01e5b8e490d45aa80f80df49743cbc9dc
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-03-26 23:44:02 +0100

    select & where

--- webprog/php - mysql/import.sql
+++ webprog/php - mysql/import.sql
@@ -1,1 +1,9 @@
 INSERT INTO `users` (`id`, `name`) VALUES (NULL, 'ali'), (NULL, 'liam'), (NULL, 'sara'), (NULL, 'nima'), (NULL, 'navid');
+
+INSERT INTO `users` (`id`, `name`) VALUES (NULL, 'ali'), (NULL, 'liam'), (NULL, 'sara'), (NULL, 'nima'), (NULL, 'navid');
+
+INSERT INTO users (name) VALUES ("alireza"),
+								("sana"),
+                                ("ali");
+
+SELECT * FROM users WHERE id > 2;
