71d39da591bfaa7267966951026610967d749abe
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-02 15:26:55 +0200

    header include

---
+++ webprog/php - mysql/final-project/include/layout/header.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>php project</title>
+
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+            crossorigin="anonymous"
+        />
+
+        <link rel="stylesheet" href="./assets/css/style.css" />
+    </head>
+
+    <body>
+        <header class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+            <div class="ms-auto">
+                <a href="index.html" class="fs-4 fw-medium link-body-emphasis text-decoration-none">sample.com</a>
+            </div>
+            <nav class="d-inline-flex mt-2 mt-md-0 me-md-auto">
+                <a class="fw-bold me-3 py-2 link-body-emphasis text-decoration-none" href="#">Nature</a>
+                <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Tourism</a>
+                <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Technology</a>
+                <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Others</a>
+            </nav>
+        </header>
