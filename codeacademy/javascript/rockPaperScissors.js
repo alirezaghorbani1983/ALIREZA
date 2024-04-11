1dec91e817e0251b3cf9ebeb719abd9f16d0df1a
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-11 17:06:20 +0200

    Function to get the computer's choice

--- codeacademy/javascript/rockPaperScissors.js
+++ codeacademy/javascript/rockPaperScissors.js
@@ -7,3 +7,19 @@ const getUserChoice = userInput => {
       console.log('Invalid choice. Please choose rock, paper, or scissors.');
     }
   };
+
+  //console.log(getUserChoice('ttt'));
+
+  //Function to get the computer's choice
+
+  const getComputerChoise = () => {
+    const randomNumber = Math.floor(Math.random()* 3);
+    switch(randomNumber){
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
