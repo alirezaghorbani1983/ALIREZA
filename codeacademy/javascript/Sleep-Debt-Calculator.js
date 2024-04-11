feacdcd647c2540edeb903b5093bd4898c352249
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-11 21:07:07 +0200

    get hours of sleep

---
+++ codeacademy/javascript/Sleep-Debt-Calculator.js
@@ -0,0 +1,24 @@
+//Sleep Debt Calculator mini-project
+
+// get hours of sleep 
+
+const getSleepHours = day => {
+    switch (day.toLowerCase()) {
+      case 'monday':
+        return 8;
+      case 'tuesday':
+        return 7;
+      case 'wednesday':
+        return 6;
+      case 'thursday':
+        return 8;
+      case 'friday':
+        return 7;
+      case 'saturday':
+        return 8;
+      case 'sunday':
+        return 7;
+      default:
+        return 'Invalid day';
+    }
+  };
