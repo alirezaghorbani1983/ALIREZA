628d23d49466c5d3b82e4693622dc1e0a34dddce
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-12 12:39:58 +0200

    global scope

--- codeacademy/javascript/main.js
+++ codeacademy/javascript/main.js
@@ -59,14 +59,27 @@
 
 //scope
 
-const city = 'New York City';
+// const city = 'New York City';
 
-const logCitySkyline = () => {
-  let skyscraper = 'Empire State Building';
-  return 'The stars over the ' + skyscraper + ' in ' + city;
+// const logCitySkyline = () => {
+//   let skyscraper = 'Empire State Building';
+//   return 'The stars over the ' + skyscraper + ' in ' + city;
+// };
+
+// console.log(logCitySkyline());
+
+//global scope
+
+const satellite = 'The Moon';
+
+const galaxy = 'The Milky Way';
+
+const stars = 'North Star';
+
+const callMyNightSky = () =>  {
+ return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy; 
 };
 
-console.log(logCitySkyline());
-
+console.log(callMyNightSky());
 
   
