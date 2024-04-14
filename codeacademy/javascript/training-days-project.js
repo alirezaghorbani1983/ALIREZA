1304e05058dff1ec8f13f5b4fb1170078fd9116d
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-14 19:08:19 +0200

    final change

--- codeacademy/javascript/training-days-project.js
+++ codeacademy/javascript/training-days-project.js
@@ -1,7 +1,7 @@
 // The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
 
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
   if (random === 0) {
     return 'Marathon';
   } else if (random === 1) {
@@ -25,7 +25,6 @@ let days;
   return days;
 };
 
-
 const name = 'Nala';
 // The scope of `name` is too tight 
 const logEvent = (name, event) => {
@@ -41,7 +40,6 @@ const logTime = (name, days) => {
 const event = getRandEvent();
 const days = getTrainingDays(event);
 // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-
 
 logEvent(name, event);
 logTime(name, days);
