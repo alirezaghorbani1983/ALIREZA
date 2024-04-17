4efaf666d1ef3518de2c101954e98c414c382d8f
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-17 17:43:25 +0200

    truthy and falsy

--- webprog/javascript/script.js
+++ webprog/javascript/script.js
@@ -699,20 +699,31 @@
 //     }
 // }
 
-const x =20;
+// const x =20;
 
-switch(x) {
-    case 10:
-        console.log('x is 10');
-        break;
-    case 15:
-        console.log('x is 15');
-        break;
-    case 20:
-        console.log('x is 20');
-        break;
-    default:
-        console.log(`x is ${x}`);
-        break;
+// switch(x) {
+//     case 10:
+//         console.log('x is 10');
+//         break;
+//     case 15:
+//         console.log('x is 15');
+//         break;
+//     case 20:
+//         console.log('x is 20');
+//         break;
+//     default:
+//         console.log(`x is ${x}`);
+//         break;
+// }
+
+// const firstName = 'alireza';
+
+// console.log(Boolean(firstName));
+
+const x = [];
+
+if(x) {
+    console.log('this is truthy');
+}else {
+    console.log('this is falsy');
 }
-
