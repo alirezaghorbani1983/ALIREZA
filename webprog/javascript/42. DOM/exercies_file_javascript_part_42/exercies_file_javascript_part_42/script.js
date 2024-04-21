4ed634af791cc297eb167b68d71a9a37be38fdd4
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-21 13:26:34 +0200

    add

---
+++ webprog/javascript/42. DOM/exercies_file_javascript_part_42/exercies_file_javascript_part_42/script.js
@@ -0,0 +1,6 @@
+// console.log(window.document);
+// console.log(document);
+// console.log(document.body);
+// console.log(document.body.innerHTML);
+
+document.body.innerHTML = "<h1>webprog</h1>";
