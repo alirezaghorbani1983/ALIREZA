55bd43527155cf054bb38b7749f6504438264851
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-21 18:28:07 +0200

    windows object

--- webprog/javascript/42/script42.js
+++ webprog/javascript/42/script42.js
@@ -3,4 +3,9 @@
 // console.log(document.body);
 // console.log(document.body.innerHTML);
 
-document.body.innerHTML = "<h1>webprog</h1>";
+// document.body.innerHTML = "<h1>webprog</h1>";
+
+// console.log(window.document);
+// console.log(document.body.innerHTML);
+
+document.body.innerHTML = "<h1> website </h1>";
