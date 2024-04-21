2495d3325e84210cdd313092f79381f6cc2d4149
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-21 18:54:00 +0200

    propreties DOM document

--- webprog/javascript/42/script42.js
+++ webprog/javascript/42/script42.js
@@ -8,4 +8,21 @@
 // console.log(window.document);
 // console.log(document.body.innerHTML);
 
-document.body.innerHTML = "<h1> website </h1>";
+// document.body.innerHTML = "<h1> website </h1>";
+
+let output;
+
+// output = document.all;
+// output = document.all[1];
+// output = document.all.length;
+// output = document.documentElement;
+
+// output = document.head;
+// output = document.body.children;
+
+// als we gebruik document.URl en we zijn op server, zullen we domein ontvangen
+// output = document.URL;
+
+output = document.body.children[0];
+
+console.log(output);
