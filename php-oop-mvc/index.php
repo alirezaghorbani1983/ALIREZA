cd5da93ec8aeff6393bcbc843dfc0501f563382d
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-26 13:20:59 +0200

    create an object

--- php-oop-mvc/index.php
+++ php-oop-mvc/index.php
@@ -10,3 +10,14 @@ class Person {
     }
     
 }
+
+
+// now we moet een object van deze class creeren
+// als we een object van deze class gaan ontwerpen, moeten we deze 
+// attribute een naam geven zoals $name   ali and $family  ghorbani
+
+$myPerson = new Person();
+
+$myPerson-> name ="Ali";
+$myPerson->family = "Ghorbani";
+$myPerson->showInfo();
