bf47356f92d227589d164b7a426a1505c2903909
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-04-28 00:44:27 +0200

    const property

--- php-oop-mvc/index.php
+++ php-oop-mvc/index.php
@@ -1,20 +1,46 @@
 <?php 
 
-class Person {
+// class Person {
 
-    public $name;
-    public $family;
+//     public $name;
+//     public $family;
 
-    public function showInfo(){
-        echo $this->name . " " . $this->family;
-    }
+//     public function showInfo(){
+//         echo $this->name . " " . $this->family;
+//     }
     
-}
+// }
 
 
 // now we moet een object van deze class creeren
 // als we een object van deze class gaan ontwerpen, moeten we deze 
 // attribute een naam geven zoals $name   ali and $family  ghorbani
+
+// $myPerson = new Person();
+
+// $myPerson-> name ="Ali";
+// $myPerson->family = "Ghorbani";
+// $myPerson->showInfo();
+
+// echo '<br>';
+
+// $myPerson2 = new Person();
+
+// $myPerson2-> name ="Reza";
+// $myPerson2->family = "Mohammadi";
+// $myPerson2->showInfo();
+
+class Person {
+
+    public $name;
+    public $family;
+    const AGE = 27;
+
+    public function showInfo(){
+        echo $this->name . " " . $this->family . " " . Person::AGE;
+    }
+    //double colon   
+}
 
 $myPerson = new Person();
 
