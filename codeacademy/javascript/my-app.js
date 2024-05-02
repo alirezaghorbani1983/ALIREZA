6e142cac3af56dd3dfae03dbed9fc6df0fbe6f52
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-03 00:22:33 +0200

    ADDED

--- codeacademy/javascript/my-app.js
+++ codeacademy/javascript/my-app.js
@@ -382,14 +382,15 @@ if (season === 'spring') {
 //   }
 // }
 
-const numberDigits = (x) => {
-  if (x >= 0 && x < 10) {
-    return 'One digit: ' + x;
-  } else if (x >= 10 && x < 100) {
-    return 'Two digits: ' + x;
-  } else {
-    return 'The number is: ' + x;
-  }
-}
+// const numberDigits = (x) => {
+//   if (x >= 0 && x < 10) {
+//     return 'One digit: ' + x;
+//   } else if (x >= 10 && x < 100) {
+//     return 'Two digits: ' + x;
+//   } else {
+//     return 'The number is: ' + x;
+//   }
+// }
 
-console.log(numberDigits(5));   
+// console.log(numberDigits(5));   
+console.log(process.env.PWD);
