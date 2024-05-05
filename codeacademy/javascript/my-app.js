325912c1f67c1c850064e72e006a0ba262a966ea
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-05 23:41:59 +0200

    Update Elements

--- codeacademy/javascript/my-app.js
+++ codeacademy/javascript/my-app.js
@@ -424,11 +424,15 @@ if (season === 'spring') {
 //   currentRoundNumber++;
 // }
 
-const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+// const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
 
-let listItem = famousSayings[0];
-console.log(listItem);
+// let listItem = famousSayings[0];
+// console.log(listItem);
 
-console.log(famousSayings[2]);
+// console.log(famousSayings[2]);
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
 
 
