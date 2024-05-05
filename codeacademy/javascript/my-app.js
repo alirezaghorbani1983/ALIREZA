fd784125604229270eed46524f60d3fb667c1030
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-05 23:19:03 +0200

    array

--- codeacademy/javascript/my-app.js
+++ codeacademy/javascript/my-app.js
@@ -396,32 +396,34 @@ if (season === 'spring') {
 // console.log('Hello, World!');
 // console.log('Hello, World!');
 // console.log('Hello, World!');
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+// let humanScore = 0;
+// let computerScore = 0;
+// let currentRoundNumber = 1;
 
-// Write your code below:
-const generateTarget = () => {
-  return Math.floor(Math.random() * 10);
-}
+// // Write your code below:
+// const generateTarget = () => {
+//   return Math.floor(Math.random() * 10);
+// }
 
-const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
-  const humanDifference = Math.abs(targetNumber - humanGuess);
-  const computerDifference = Math.abs(targetNumber - computerGuess);
+// const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+//   const humanDifference = Math.abs(targetNumber - humanGuess);
+//   const computerDifference = Math.abs(targetNumber - computerGuess);
   
- return humanDifference <= computerDifference;
-}
+//  return humanDifference <= computerDifference;
+// }
 
-const updateScore = winner => {
-  if (winner === 'human') {
-    humanScore++;
-  } else if (winner === 'computer') {
-    computerScore++;
-  }
-}
+// const updateScore = winner => {
+//   if (winner === 'human') {
+//     humanScore++;
+//   } else if (winner === 'computer') {
+//     computerScore++;
+//   }
+// }
 
-const advanceRound = () => {
-  currentRoundNumber++;
-}
+// const advanceRound = () => {
+//   currentRoundNumber++;
+// }
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
 
 
