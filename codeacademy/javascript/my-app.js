fdfeabdaeeea7d361f0309c53d176271cb0131a0
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-05 23:57:45 +0200

    Arrays with let and const

--- codeacademy/javascript/my-app.js
+++ codeacademy/javascript/my-app.js
@@ -431,8 +431,15 @@ if (season === 'spring') {
 
 // console.log(famousSayings[2]);
 
-let groceryList = ['bread', 'tomatoes', 'milk'];
+// let groceryList = ['bread', 'tomatoes', 'milk'];
 
-groceryList[1] = 'avocados';
+// groceryList[1] = 'avocados';
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
 
 
