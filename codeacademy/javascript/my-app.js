74db32b93c0cbbd7bb3c9d34d32aca4b18fae178
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-05 23:24:54 +0200

    Accessing Elements

--- codeacademy/javascript/my-app.js
+++ codeacademy/javascript/my-app.js
@@ -426,4 +426,9 @@ if (season === 'spring') {
 
 const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
 
+let listItem = famousSayings[0];
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
 
