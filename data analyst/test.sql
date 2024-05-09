27706582f42543c73a0570e7db0c2b32bf689fea
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-09 18:40:21 +0200

    CREATE table

--- data analyst/test.sql
+++ data analyst/test.sql
@@ -2,34 +2,50 @@
 
 -- create table
 
-CREATE TABLE celebs (
-   id INTEGER, 
-   name TEXT, 
-   age INTEGER
-); 
+-- CREATE TABLE celebs (
+--    id INTEGER, 
+--    name TEXT, 
+--    age INTEGER
+-- ); 
 
-INSERT INTO celebs (id, name, age) 
-VALUES (1, 'Justin Bieber', 29); 
+-- INSERT INTO celebs (id, name, age) 
+-- VALUES (1, 'Justin Bieber', 29); 
 
-INSERT INTO celebs (id, name, age) 
-VALUES (2, 'Beyonce Knowles', 42); 
+-- INSERT INTO celebs (id, name, age) 
+-- VALUES (2, 'Beyonce Knowles', 42); 
 
-INSERT INTO celebs (id, name, age) 
-VALUES (3, 'Jeremy Lin', 35); 
+-- INSERT INTO celebs (id, name, age) 
+-- VALUES (3, 'Jeremy Lin', 35); 
 
-INSERT INTO celebs (id, name, age) 
-VALUES (4, 'Taylor Swift', 33); 
+-- INSERT INTO celebs (id, name, age) 
+-- VALUES (4, 'Taylor Swift', 33); 
 
--- SELECT name FROM celebs; 
-SELECT * FROM celebs;
+-- -- SELECT name FROM celebs; 
+-- SELECT * FROM celebs;
 
-DELETE FROM celebs 
-WHERE twitter_handle IS NULL;
+-- DELETE FROM celebs 
+-- WHERE twitter_handle IS NULL;
 
-SELECT * FROM celebs; 
+-- SELECT * FROM celebs; 
 
-CREATE TABLE awards (
-   id INTEGER PRIMARY KEY,
-   recipient TEXT NOT NULL,
-   award_name TEXT DEFAULT 'Grammy'
+-- CREATE TABLE awards (
+--    id INTEGER PRIMARY KEY,
+--    recipient TEXT NOT NULL,
+--    award_name TEXT DEFAULT 'Grammy'
+-- );
+
+//MINI PROJECT
+
+CREATE TABLE friends (
+    id INTEGER,
+    name TEXT,
+    birthday DATE
 );
+
+INSERT INTO friends (name, birthday) VALUES ('Ororo Munroe', '1940-05-30');
+
+SELECT * FROM friends;
+
+INSERT INTO friends (id, name, birthday) VALUES (1, 'John Doe', '1990-04-15');
+
+INSERT INTO friends (id, name, birthday) VALUES (2, 'Jane Smith', '1985-10-20');
