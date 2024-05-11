c298915d814fb9bdca6b6c3056dc0cb288838e38
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-05-11 21:19:40 +0200

    calculation

---
+++ data analyst/python/script.py
@@ -0,0 +1,26 @@
+print("This message has mismatched quote marks!")
+print('Abracadabra')
+
+
+ below:
+runtime = 95;
+release_year = 1999;
+
+
+
+rating_out_of_10 = 5.8;
+
+#quilt
+
+#First we create the variables
+quilt_width = 8;
+quilt_length = 12;
+
+#Then we print the size
+print(quilt_width * quilt_length);
+
+#Whoops! That's a little too large
+quilt_length = 8;
+
+#Let's see how large it is now
+print(quilt_width * quilt_length);
