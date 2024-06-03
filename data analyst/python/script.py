c3f6ed3e65d40a4b20750e76ae6b5b776f1f18b8
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-03 20:28:09 +0200

    exponents

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -24,3 +24,17 @@ quilt_length = 8;
 
 #Let's see how large it is now
 print(quilt_width * quilt_length);
+
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+
+# 8 squared, or 64
+print(8 ** 2)
+
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
