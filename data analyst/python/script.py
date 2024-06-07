49d9f350d7939abe6a237ed506eb5d9db331c0c4
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-07 14:35:09 +0200

    summary

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -96,3 +96,15 @@ And why should I not speak to you?
 
 print(to_you)
 
+my_age = 40
+half_my_age = my_age / 2
+greeting = "Hello"
+name = "Alireza"
+greeting_with_name = greeting + ", " + name + "!"
+
+print("My age:", my_age)
+print("Half my age:", half_my_age)
+print("Greeting:", greeting)
+print("Name:", name)
+print("Greeting with name:", greeting_with_name)
+
