78f2fe9ffc4133709305aa81276443354b07f6a6
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-07 14:29:37 +0200

    multi-line strings

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -87,3 +87,12 @@ total_price += fun_books
 
 print("The total price is", total_price)
 
+# Assign the string here
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
+"""
+
+print(to_you)
+
