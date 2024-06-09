2eec40d82c93e5686dcb2556e7fbd14ad766bcbe
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-09 14:26:06 +0200

    smoking factor

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -183,3 +183,15 @@ new_insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_chi
 change_in_insurance_cost = new_insurance_cost - insurance_cost
 
 print("The change in estimated cost for being male instead of female is " + str(change_in_insurance_cost) + " dollars.")
+
+# Smoker Factor
+smoker = 1
+
+# Calculate the new insurance cost with the updated smoker value
+new_insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_children) + (24000 * smoker) - 12500
+
+# Calculate the change in insurance cost due to smoking
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+# Display the change in insurance cost
+print("The change in estimated insurance cost for being a smoker is " + str(change_in_insurance_cost) + " dollars.")
