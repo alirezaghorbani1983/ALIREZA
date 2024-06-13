16ea992d3776210b0eaaf3f20d3b9ccc1640f8cc
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-13 21:29:11 +0200

    insurance cost estimation

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -331,3 +331,41 @@ print(most_popular1)
 print(most_popular2)
 print(most_popular3)
 
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+
+trip_planner_welcome("Ali")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(3.6)
+
+# Create calculate_insurance_cost() function below: 
+
+
+# Initial variables for Maria 
+age = 28
+sex = 0  
+bmi = 26.2
+num_of_children = 3
+smoker = 0  
+
+# Estimate Maria's insurance cost
+insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+
+print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
+
+# Initial variables for Omar
+age = 35
+sex = 1 
+bmi = 22.2
+num_of_children = 0
+smoker = 1  
+
+# Estimate Omar's insurance cost 
+insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+
+print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
+
