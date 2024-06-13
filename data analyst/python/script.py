bd67c179daf6aae447097a8d4e45ea322326711a
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-13 17:42:46 +0200

    Multiple Returns

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -285,6 +285,8 @@ print(min_price)
 rounded_price = round(tshirt_price, 1)
 
 print(rounded_price)
+
+
 def my_function():
  x = 200
  print(x)
@@ -299,4 +301,33 @@ def outer_function():
   inner_function()
 
 outer_function()
+
+current_budget = 3500.75
+
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+
+print_remaining_budget(current_budget)
+
+# Write your code below: 
+def deduct_expense(budget, expense):
+  return (budget - expense)
+
+shirt_expense = 9
+
+new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
+
+print_remaining_budget(new_budget_after_shirt)
+
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third
+
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
 
