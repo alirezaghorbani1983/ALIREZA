08b47e7797a5ced221051b99d03ad453fd9c5089
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-15 21:30:59 +0200

    Relational Operators

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -441,3 +441,17 @@ user_name = "angela_catlady_87"
 if user_name == "angela_catlady_87":
   print("I know it is you, Dave! Go away!")
 
+  x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+  print("These numbers are the same")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+  print("You have enough credits to graduate!")
+
