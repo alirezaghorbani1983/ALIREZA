f959e667e67819527256ae0bf717d85089fa9157
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-24 23:57:21 +0200

    pop() function

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -701,4 +701,16 @@ front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
 front_display_list.insert(0, "Pineapple")
 print(front_display_list)
 
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+# print(data_science_topics)
+
+# Your code below: 
+
+data_science_topics.pop()
+
+print(data_science_topics)
+
+data_science_topics.pop(3)
+print(data_science_topics)
+
 
