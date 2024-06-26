3236028034a167f398a429968d104ca95c2b3aef
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-26 11:14:04 +0200

    slicing

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -738,5 +738,15 @@ big_range = range(2, 3000, 100)
 big_range_length = len(big_range)
 print(big_range_length)
 
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:2]
+
+# Your code below: 
+print(beginning)
+
+middle = suitcase[2:4]
+print(middle)
+
 
 
