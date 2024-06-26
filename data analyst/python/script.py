fcb88d40e55a7bc4b9bd1b79d6a835f81bbd4178
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-26 21:15:30 +0200

    count function

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -748,5 +748,11 @@ print(beginning)
 middle = suitcase[2:4]
 print(middle)
 
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Your code below: 
+jake_votes = votes.count("Jake")
+print(jake_votes)
+
 
 
