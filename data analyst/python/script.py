5bcb087bfd99aece90def379531629f13095f99e
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-27 11:33:33 +0200

    sort()

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -754,5 +754,16 @@ votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "
 jake_votes = votes.count("Jake")
 print(jake_votes)
 
+# Checkpoint 1 & 2
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+
+addresses.sort()
+print(addresses)
+
+# Checkpoint 3
+names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+names.sort()
+print(names)
+
 
 
