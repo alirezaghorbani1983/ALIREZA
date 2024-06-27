b4eeda55ee20a1409474dc9f7a995446224f33b1
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-06-27 12:03:16 +0200

    sorted function

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -765,5 +765,13 @@ names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
 names.sort()
 print(names)
 
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+# Your code below:
+games_sorted = sorted(games)
+
+print(games)
+print(games_sorted)
+
 
 
