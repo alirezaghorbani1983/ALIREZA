67d05baf807ce6f86e7cae5e421895d47d5dab9d
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-07-03 18:49:44 +0200

    Loop Control with Break

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -856,3 +856,15 @@ while index < length:
 for student in students_period_A:
   # students_period_A.append(student)
   print(student)
+
+
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+    print(dog_breed)
+    if dog_breed == dog_breed_I_want:
+        print("They have the dog I want!")
+        break
+
+print("End of search!")
