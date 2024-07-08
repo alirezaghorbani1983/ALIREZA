588db773754634bad5dd3b2292541c05efd1ec95
alireza <alirezaghorbani.bmedu@yahoo.com>
2024-07-08 12:07:15 +0200

    List Comprehensions: Conditionals

--- data analyst/python/script.py
+++ data analyst/python/script.py
@@ -896,3 +896,10 @@ scaled_grades = [grade + 10 for grade in grades]
 
 # Print the new list of scaled grades
 print(scaled_grades)
+
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+
+# Create a new list called can_ride_coaster using a list comprehension
+can_ride_coaster = [height for height in heights if height > 161]
+
+print(can_ride_coaster)
